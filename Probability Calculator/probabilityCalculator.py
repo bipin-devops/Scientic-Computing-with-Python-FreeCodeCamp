b80f6f98936464270a7750c4c2c1d26334bbf301
bipin-devops <viraatsitoula@gmail.com>
2025-02-01 04:10:23 +1100

    Implemented experiment function in probability Calculator.py; project completec

--- Probability Calculator/probabilityCalculator.py
+++ Probability Calculator/probabilityCalculator.py
@@ -2,6 +2,7 @@ import copy
 import random
 
 class Hat:
+    
     contents: list
     
     def __init__(self, **kwargs):
@@ -12,8 +13,9 @@ class Hat:
             all_balls = self.contents.copy()
             self.contents = []
             return all_balls
-            
-        random_elements = random.choices(self.contents, k = number)
+        
+        # use sample instead of choices as we need without replacement
+        random_elements = random.sample(self.contents, k = number)
         
         # dictionary to count occurence in random_element
         count_to_remove = {}
@@ -35,16 +37,47 @@ class Hat:
             self.contents.pop(index)
         return random_elements
 
-
-        
-
-    
-
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    probability: float
-    hat_copy = copy.copy(hat)
+    # expected_balls: An object indicating the exact group of balls to attempt to draw from the hat for the experiment. {}
     
-    
+    successful_experiments = 0
+
+    for i in range(num_experiments):
+        # copy the hat for this experiment
+        hat_copy = copy.deepcopy(hat)
+
+        # draw the ball
+        balls_drawn = hat_copy.draw(num_balls_drawn)
+
+        # count the balls drawn
+        count_balls_drawn = {}
+        for ball in balls_drawn:
+            if ball in count_balls_drawn:
+                count_balls_drawn[ball] += 1
+            else:
+                count_balls_drawn[ball] = 1
+        
+        # verify if we got the expected balls or not
+        success = True
+        for ball, count in expected_balls.items():
+            if ball not in count_balls_drawn or count_balls_drawn[ball] < count:
+                success = False
+                break
+        
+        if success:
+            successful_experiments += 1
+
+    probability = successful_experiments / num_experiments
+    return probability 
+
 
 hat1 = Hat(yellow=3, blue=2, green=6)
 hat1.draw(15)
+
+hat = Hat(black=6, red=4, green=3)
+probability = experiment(hat=hat,
+                  expected_balls={'red':2,'green':1},
+                  num_balls_drawn=5,
+                  num_experiments=2000)
+
+print(f'Probability: {probability}' )
